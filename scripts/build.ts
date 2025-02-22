d1b16c9750f51a4fb93e56c4431613a51aefcdb7
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-02-22 19:06:00 +0300

    chore: rewritted typebox logic with typebox-genx

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,14 +1,23 @@
+import path from "node:path";
 import { $ } from "bun";
+import { GenX } from "@toil/typebox-genx";
 
 import { name } from "../package.json";
-import { generateTypebox } from "./typebox-gen";
 
 async function build() {
   console.log(`Building ${name}...`);
   $.cwd("./");
   await $`rm -rf dist`;
   await $`tsc --project tsconfig.build.json --outdir ./dist && tsc-alias -p tsconfig.build.json && tsc-esm-fix --tsconfig tsconfig.build.json`;
-  await generateTypebox("./");
+  const packagePath = path.join(__dirname, "..");
+  const genx = new GenX({
+    root: packagePath,
+  });
+  await $`mkdir dist/typebox`;
+  await genx.generateByDir(
+    path.resolve(packagePath, "src", "types"),
+    path.resolve(packagePath, "dist", "typebox"),
+  );
   $.cwd("./");
 }
 
