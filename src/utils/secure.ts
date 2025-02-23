6b583c64bddb3029231968d5c38933566903dc39
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-02-23 14:56:35 +0300

    chore: added gen uuid v4

--- src/utils/secure.ts
+++ src/utils/secure.ts
@@ -10,3 +10,11 @@ export function getUUID() {
   }
   return uuid;
 }
+
+export function generateUUIDv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (char) => {
+    const random = (Math.random() * 16) | 0;
+    const value = char === "x" ? random : (random & 0x3) | 0x8;
+    return value.toString(16);
+  });
+}
